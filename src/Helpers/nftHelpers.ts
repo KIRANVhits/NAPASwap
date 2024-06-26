00df586f81253e03e388162e15e0554c4e1dd999
KiranMTomar <kirantomar456@gmail.com>
2024-06-26 12:34:22 +0530

    Change | api key

--- src/Helpers/nftHelpers.ts
+++ src/Helpers/nftHelpers.ts
@@ -5,7 +5,7 @@ import { EvmChain } from "@moralisweb3/common-evm-utils";
 // D8Kfm2KtjFHVEpqvPmTVgaNLvY8TFEhrIBi8h71wjcTfFIdlmSKFlYJcEGATK8dr
 export const runMoralis = async () => {
   await Moralis.start({
-    apiKey: "gxFx0RWobr82DUQZD7W2qwtsfaW63p6QtIJh7pZSvRshWexNbv58m9Dc1hai9ZLl",
+    apiKey: "D8Kfm2KtjFHVEpqvPmTVgaNLvY8TFEhrIBi8h71wjcTfFIdlmSKFlYJcEGATK8dr",
   });
 };
 
@@ -13,7 +13,7 @@ export const getNapaNFTOfUser = async (currentAccount: string) => {
   //   const address = "0xd4e4078ca3495DE5B1d4dB434BEbc5a986197782";
 
   const response = await Moralis.EvmApi.nft.getWalletNFTs({
-    chain: "0x5",
+    chain: "0xaa36a7",
     format: "decimal",
     tokenAddresses: ["0xE7D1532f5B616c26C0CD8952b2a54D8DCbfa9932"],
     mediaItems: false,
